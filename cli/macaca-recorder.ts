17beb48d97d9a3a67cf8a24425a233acda237f3b
ilimei <514222057@qq.com>
2022-11-04 09:24:34 +0800

    feat: cli add options support

--- cli/macaca-recorder.ts
+++ cli/macaca-recorder.ts
@@ -1,4 +1,4 @@
-import { program } from 'commander';
+import { program, Command } from 'commander';
 import { EOL } from 'os';
 import {
   ApiPlugin,
@@ -12,35 +12,56 @@ import {
 } from '../src';
 import pkg from '../package.json';
 
+export type CodeEngineType = 'macaca' | 'sky' | 'editor';
+
+// eslint-disable-next-line consistent-return
+function getEngineType(engine: string, cmd: Command): CodeEngineType {
+  if (['macaca', 'sky', 'editor'].includes(engine)) {
+    return engine as CodeEngineType;
+  }
+  cmd.addHelpText('before', `${EOL}unknown engine ${engine}${EOL}`);
+  cmd.help();
+}
+
 // 启动录制命令
 const recorderCommand = program
   .createCommand('run')
+  .option(
+    '-e, --engine <engine>',
+    'gen code engine, engines: macaca, sky, editor',
+    'macaca',
+  )
+  .option('-h, --highlight', 'show highlight', false)
   .argument('<url>', 'recorder url')
   .showHelpAfterError(true)
-  .action(async (url: string) => {
-    const app = await App.createApp(
-      [
-        ApiPlugin,
-        IOptionPlugin,
-        EventPlugin,
-        GeneratorPlugin,
-        BrowserPlugin,
-        UIStatePlugin,
-      ],
-      iapiID,
-    );
+  .action(
+    async (
+      url: string,
+      options: { engine: string; highlight: boolean },
+      cmd: Command,
+    ) => {
+      const app = await App.createApp(
+        [
+          ApiPlugin,
+          IOptionPlugin,
+          EventPlugin,
+          GeneratorPlugin,
+          BrowserPlugin,
+          UIStatePlugin,
+        ],
+        iapiID,
+      );
 
-    // 设置代码生成引擎
-    // app.options.setRecorderEngine('editor').setShowHighlight(false);
-    app.options
-      .setRecorderEngine('macaca')
-      .setShowHighlight(false)
-      .setStartRecordOnFirst(true);
+      app.options
+        .setRecorderEngine(getEngineType(options.engine, cmd))
+        .setShowHighlight(options.highlight)
+        .setStartRecordOnFirst(true);
 
-    await app.init();
+      await app.init();
 
-    await app.codeGen.start(url);
-  });
+      await app.codeGen.start(url);
+    },
+  );
 
 program
   .option('-v, --version', 'show version and exit')
