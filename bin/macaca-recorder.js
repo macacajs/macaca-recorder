fc96749363b8e01bd30532568d4f5e51398bfaf9
ilimei <514222057@qq.com>
2022-11-04 11:06:00 +0800

    fix: cmd require error

--- bin/macaca-recorder.js
+++ bin/macaca-recorder.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 const path = require('path');
+const projectURL = path.join(__dirname, '../');
 
 require('ts-node').register({
-  project: path.join(__dirname, '../'),
+  project: projectURL,
+});
+require('tsconfig-paths').register({
+  cwd: projectURL,
 });
 // 启动
 require('../cli/macaca-recorder');
